63bd11b716fa5c44ab0a68ac186d347bc7d3897a
wilson-programmer <78704068+wilson-programmer@users.noreply.github.com>
2024-05-27 22:37:13 +0200

    Update bench_redis.sh
    modification of number of requests in redis benchmark

--- redis_bench/bench_redis.sh
+++ redis_bench/bench_redis.sh
@@ -2,7 +2,7 @@
 
 /etc/init.d/redis-server start
 
-redis-benchmark -n 1000000 -c 100 --csv -d 128 -P 8 >> redis_bench_result.csv
+redis-benchmark -n 100000 -c 100 --csv -d 128 -P 8 >> redis_bench_result.csv
 
 #rediriger les résultats dans les dossiers correspondants
 
